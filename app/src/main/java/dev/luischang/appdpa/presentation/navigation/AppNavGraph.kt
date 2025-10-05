fa3c91163c8e43f11f26e1da6e61ba5690c4356a
Luis Chang <luis.chang@outlook.com>
2025-10-04 19:20:45 -0500

    DrawerScaffold in AppNavGraph

--- app/src/main/java/dev/luischang/appdpa/presentation/navigation/AppNavGraph.kt
+++ app/src/main/java/dev/luischang/appdpa/presentation/navigation/AppNavGraph.kt
@@ -1,5 +1,6 @@
 package dev.luischang.appdpa.presentation.navigation
 
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -11,12 +12,25 @@ import dev.luischang.appdpa.presentation.home.HomeScreen
 @Composable
 fun AppNavGraph(){
     val navController = rememberNavController()
-
     NavHost(navController = navController, startDestination = "home")
     {
         composable("register") { RegisterScreen(navController) }
         composable("login") { LoginScreen(navController) }
-        composable("home") { HomeScreen() }
-    }
 
+        composable("home") {
+            DrawerScaffold(navController) {
+                HomeScreen()
+            }
+        }
+        composable ("permissions") {
+            DrawerScaffold(navController) {
+                Text("Proximamente pantalla permisos.....")
+            }
+        }
+        composable ("favorites") {
+            DrawerScaffold(navController) {
+                Text("Proximamente pantalla favoritos.....")
+            }
+        }
+    }
 }
