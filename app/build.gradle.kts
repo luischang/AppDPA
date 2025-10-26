a5891ce4f7e36b556501424d61820ea635968702
Luis Chang <luis.chang@outlook.com>
2025-10-25 19:38:17 -0500

    ApiFootballService

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,4 +67,13 @@ dependencies {
 
     //Firebase BoM
     implementation(platform("com.google.firebase:firebase-bom:34.3.0"))
+
+    //Retrofit
+    //implementation("com.squareup.retrofit2:retrofit:3.1.0-SNAPSHOT")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.9.0")
+
+
+
 }
---
+++ app/src/main/java/dev/luischang/appdpa/data/remote/apifootball/ApiFootballService.kt
@@ -0,0 +1,15 @@
+package dev.luischang.appdpa.data.remote.apifootball
+
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface ApiFootballService {
+
+    @GET("countries")
+    suspend fun getCountries(): CountryResponse
+
+    @GET("teams")
+    suspend fun getTeamsByCountry(@Query("country") country: String )
+                : TeamResponse
+
+}
