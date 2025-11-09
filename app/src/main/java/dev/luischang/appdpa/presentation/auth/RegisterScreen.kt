acb26a8ad7e47115a440c94fdc5b005bbbb5e2b1
Luis Chang <luis.chang@outlook.com>
2025-11-08 21:21:26 -0500

    Terms and conditions in RegisterScreen

--- app/src/main/java/dev/luischang/appdpa/presentation/auth/RegisterScreen.kt
+++ app/src/main/java/dev/luischang/appdpa/presentation/auth/RegisterScreen.kt
@@ -1,23 +1,31 @@
 package dev.luischang.appdpa.presentation.auth
 
+import android.webkit.WebView
 import android.widget.Toast
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
 import androidx.navigation.NavController
 import dev.luischang.appdpa.data.remote.firebase.FirebaseAuthManager
 import kotlinx.coroutines.CoroutineScope
@@ -31,6 +39,10 @@ fun RegisterScreen(navController: NavController){
     var email by remember {mutableStateOf("")}
     var password by remember {mutableStateOf("")}
     var confirmPassword by remember {mutableStateOf("")}
+
+    var acceptTerms by remember {mutableStateOf(false)}
+    var showTerms by remember {mutableStateOf(false)}
+
 
     val context = LocalContext.current
 
@@ -69,6 +81,18 @@ fun RegisterScreen(navController: NavController){
             modifier = Modifier.padding(vertical = 8.dp).fillMaxWidth(),
             visualTransformation = PasswordVisualTransformation()
         )
+        Spacer(modifier = Modifier.padding(16.dp))
+        Row(verticalAlignment = Alignment.CenterVertically){
+            Checkbox(
+                checked = acceptTerms,
+                onCheckedChange = { acceptTerms = it }
+            )
+            Button(onClick = { showTerms = true }){
+                Text("Acepto los términos y condiciones")
+            }
+        }
+
+
         //Button for register
         Button(
             onClick = {
@@ -93,6 +117,31 @@ fun RegisterScreen(navController: NavController){
             modifier = Modifier.padding(vertical = 8.dp).fillMaxWidth()
         ) {
             Text("Registrar")
+        }
+
+        if(showTerms){
+            AlertDialog(
+                onDismissRequest = { showTerms = false },
+                confirmButton =  {
+                    TextButton(onClick = { showTerms = false }) {
+                        Text("Cerrar")
+                    }
+                },
+                title = { Text("Términos y condiciones") },
+                text = {
+                    //WebView
+                    AndroidView(
+                        factory = { context ->
+                            WebView(context).apply {
+                                settings.javaScriptEnabled = true
+                                settings.domStorageEnabled = true
+                                loadUrl("https://www.privacypolicies.com/live/23251f1a-b7d8-45e9-9485-5ea09c783c85")
+                            }
+                        },
+                        modifier = Modifier.height(300.dp)
+                    )
+                }
+            )
         }
 
     }
